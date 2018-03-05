f63f676c5216f5476e05ad74e7767c0522e2aaa4
marcbgold <marcbgold@yahoo.com>
2018-03-05 10:27:52 -0500

    updated one image

--- src/main/java/org/wecancodeit/reviewsFullStack/ReviewPopulator.java
+++ src/main/java/org/wecancodeit/reviewsFullStack/ReviewPopulator.java
@@ -34,7 +34,7 @@ public class ReviewPopulator implements CommandLineRunner {
 
 		reviewRepo.save(new Review(rpg, "Chrono Trigger", new Date(), 1995, "Time-hopping shenanigans.", "./images/chrono-trigger.jpg", "Mute, spiky-haired kid",
 				"Saves the world from space monster", "By messing with time", snes, twoD, storyDriven));
-		reviewRepo.save(new Review(platformer, "Crash Bandicoot", new Date(), 1996, "Rodent thing with pants vs. midget mad scientist.", "./images/crash-bandicoot.png",
+		reviewRepo.save(new Review(platformer, "Crash Bandicoot", new Date(), 1996, "Rodent thing with pants vs. midget mad scientist.", "./images/crash-bandicoot.jpg",
 				"Running towards the screen", "Falling into unseen holes", "The gamedevs hate you", psx, threeD));
 		reviewRepo.save(new Review(platformer, "Portal 2", new Date(), 2011, "Free cake.  So delicious and moist.", "./images/portal.jpg", "Here's the test result:",
 				"\"You're a horrible person.\"", "See? That's what it says.", pc, threeD, puzzle, storyDriven));
